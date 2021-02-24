1581f3b80eec6cc9eab2ecf569c1f6b2007867f0
ZawadiSumba66 <rosesumba98@gmail.com>
2021-02-24 17:05:17 +0300

    Add forign key and class_name to event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_124108) do
+ActiveRecord::Schema.define(version: 2021_02_24_132045) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "event_name"
+    t.date "event_date"
+    t.text "event_description"
+    t.string "location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
