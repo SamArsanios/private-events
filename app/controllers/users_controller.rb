e9150d0f03cf832843e3b1b51f443fe67dbcf74a
SamArsanios <samson.kibrom@hotmail.com>
2021-02-25 17:32:26 +0300

    Add sign_up and sign_in methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,24 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
 
+    def sign_in
+        @user = User.find(name: params[:name])
+        
+        if @user
+            session[:id] = @user.id
+            session[:name] = @user.name
+            redirect_to @user
+        else
+            render :sign_in
+        end
+    end
+
+    def sign_out
+        session.delete(:id)
+        session.delete(:name)
+        redirect_to root_path
+    end
+
     private 
        
     def user_params
