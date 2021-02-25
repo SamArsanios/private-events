8a86179d68d795d8c17a0013697c171c4a0e55de
SamArsanios <samson.kibrom@hotmail.com>
2021-02-25 15:50:04 +0300

    Add helper method/logic for diplaying events in view

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,9 @@
 module EventsHelper
+    def display_events(events)
+        events_names = ''
+        events.each do |event|
+            events_names << (content_tag :p, (link_to event.event_name, event))
+        end        
+    end
 end
+
