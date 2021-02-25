4ee147a8136ffcf11382ddbedee64b63e2155f9d
SamArsanios <samson.kibrom@hotmail.com>
2021-02-25 15:49:14 +0300

    Use scopes to display past and upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
     has_many :invitations, foreign_key: :attended_event_id
     has_many :atendees, through: :invitations, class_name: "User"
     belongs_to :creator ,foreign_key: 'creator_id' ,class_name: "User" 
+
+    scope :past, -> (date = Date.today.to_s) { where('event_date < ?',date)}
+    scope :upcoming, -> (date = Date.today.to_s) { where('event_date >= ?',date)}
 end
