9e8ed3fa50998eb27aee2c699069047523a4851f
SamArsanios <samson.kibrom@hotmail.com>
2021-02-26 08:43:31 +0300

    Modify routes, modify views

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,9 +1,18 @@
 module EventsHelper
+    def display_attendees
+        names = ''
+        @event.atendees.each do |atendee|
+            names << (content_tag :p, (link_to atendee.name, atendee))
+        end
+        names.html_safe
+    end
+
     def display_events(events)
         events_names = ''
         events.each do |event|
             events_names << (content_tag :p, (link_to event.event_name, event))
-        end        
+        end      
+       events_names.html_safe  
     end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'events#index'
+  get 'users/sign_in', to: 'users#sign_in'
+  put 'users/sign_up', to: 'users#sign_up'
+
+  resources :users, only: %i[new create show]
+  resources :events
 end
