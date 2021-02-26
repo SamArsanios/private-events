155841d5b250f227738e21424bebf2b9bb70c00e
SamArsanios <samson.kibrom@hotmail.com>
2021-02-26 16:10:09 +0300

    Fix linter files

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 RSpec.configure do |c|
-    c.use_transactional_examples = false
-    c.order = 'defined'
+  c.use_transactional_examples = false
+  c.order = 'defined'
 end
-  
