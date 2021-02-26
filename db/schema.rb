3c955099fa86ca1076fb33f24b3f9fa33c0bf6d6
SamArsanios <samson.kibrom@hotmail.com>
2021-02-26 08:40:29 +0300

    Add creator_id to events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_094304) do
+ActiveRecord::Schema.define(version: 2021_02_25_152550) do
 
   create_table "events", force: :cascade do |t|
     t.string "event_name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_02_25_094304) do
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator_id"
   end
 
   create_table "invitations", force: :cascade do |t|
