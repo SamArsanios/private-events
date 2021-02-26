f09b518c3f432c6041709805d9b5bae1e0afb81d
SamArsanios <samson.kibrom@hotmail.com>
2021-02-26 08:41:45 +0300

    Modify models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
     has_many :invitations, foreign_key: :attended_event_id
     has_many :atendees, through: :invitations, class_name: "User"
-    belongs_to :creator ,foreign_key: 'creator_id' ,class_name: "User" 
+    belongs_to :creator ,class_name: "User" 
 
     scope :past, -> (date = Date.today.to_s) { where('event_date < ?',date)}
     scope :upcoming, -> (date = Date.today.to_s) { where('event_date >= ?',date)}
